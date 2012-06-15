38bfb9fede126c53769986a17cfcdcd1d2ebe9b5
Dozier Hudson <me@dozierhudson.com>
2012-06-15 00:55:55 -0400

    Added node.js example.

---
+++ examples/nodejs/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express.createServer(express.logger());
+var webpipe = require('../../webpipe');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	
+app.use(express.bodyParser());
+
+app.get('/', function(request, response) {
+	webpipe.manual('proxy', function (er, data) {
+		if (er) {
+			console.log(er);
+			response.send("Error: "  + er);
+		} else {
+			console.log(data);
+			response.send(data);
+		}
+	});
+});
+
+var port = process.env.PORT || 3020;
+app.listen(port, function() {
+	console.log("Webpipe.js Demo: Listening on " + port);
+});
