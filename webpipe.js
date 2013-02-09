9db0e3bccf5e0bb407565482885bcd405e5110e3
Matthew Hudson <matt@matthewghudson.com>
2013-02-09 14:18:35 -0500

    Fixed indentation style.

--- webpipe.js
+++ webpipe.js
@@ -1,57 +1,62 @@
-(function(exports) {
+(function (exports) {
 
-exports.options = function(url, callback) {
+  exports.options = function (url, callback) {
     return exports.request("OPTIONS", url, null, function (err, meta) {
-        if (err) {
-            callback(err, meta);
-        } else {
-            if (!meta.url) {
-                meta.url = url;
-            }
-            if (meta.url !== url) {
-                console.warn("URLs don't match: " + url + " " + meta.url);
-            }
-            callback(null, meta);
+      if (err) {
+        callback(err, meta);
+      } else {
+        if (!meta.url) {
+          meta.url = url;
         }
+        if (meta.url !== url) {
+          console.warn("URLs don't match: " + url + " " + meta.url);
+        }
+        callback(null, meta);
+      }
     });
-}
+  }
 
-exports.execute = function(url, inputs, callback) {
-    return exports.request("POST", url, { inputs: [inputs] }, callback);
-}
+  exports.execute = function (url, inputs, callback) {
+    return exports.request("POST", url, {
+      inputs: [inputs]
+    }, callback);
+  }
 
-exports.request = function(method, url, body, callback) {
+  exports.request = function (method, url, body, callback) {
     var req = newXHR();
     req.open(method, url, true);
-    req.onreadystatechange = function() {
-        if (req.readyState === 4) {
-            if (req.status === 200) {
-                try {
-                    object = JSON.parse(req.responseText);
-                } catch (err) {
-                    return callback(err, null);
-                }
-                return callback(null, object);
-            } else {
-                callback({ code: req.status, message: req.statusText }, null);
-            }
+    req.onreadystatechange = function () {
+      if (req.readyState === 4) {
+        if (req.status === 200) {
+          try {
+            object = JSON.parse(req.responseText);
+          } catch (err) {
+            return callback(err, null);
+          }
+          return callback(null, object);
+        } else {
+          callback({
+            code: req.status,
+            message: req.statusText
+          }, null);
         }
+      }
     }
     if (body) {
-        req.send(JSON.stringify(body));
+      req.send(JSON.stringify(body));
     } else {
-        req.send(null);
+      req.send(null);
     }
-}
+  }
 
-function newXHR() {
+  function newXHR() {
     if (typeof ActiveXObject !== "undefined") {
-        return new ActiveXObject("Microsoft.XMLHTTP");
+      return new ActiveXObject("Microsoft.XMLHTTP");
     } else if (typeof XMLHttpRequest !== "undefined") {
-        return new XMLHttpRequest();
+      return new XMLHttpRequest();
     } else if (typeof require !== "undefined") {
-        return new (require("xmlhttprequest").XMLHttpRequest);
+      return new(require("xmlhttprequest").XMLHttpRequest);
     }
-}
+  }
 
 })(typeof exports !== "undefined" ? exports : (webpipe = {}));
